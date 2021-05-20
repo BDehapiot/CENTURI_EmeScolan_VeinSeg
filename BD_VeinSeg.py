538b216af78df1781370348e077171f99d8e6e9b
Benoit Dehapiot <b.dehapiot@gmail.com>
2021-05-20 18:42:06 +0200

    update 20/05/2021

--- BD_VeinSeg.py
+++ BD_VeinSeg.py
@@ -1,44 +1,46 @@
 #%% Initialize
-import napari
 import numpy as np
-import matplotlib.pyplot as plt
-
-from joblib import Parallel, delayed  
 
 from skimage import io
 from skimage.util import invert
 from skimage.filters import gaussian
-from skimage.morphology import remove_small_objects, skeletonize, disk
-from skimage.filters.rank import maximum
+from skimage.morphology import remove_small_objects, disk
 
 from scipy.ndimage import maximum_filter
 from scipy.ndimage.morphology import distance_transform_edt
 
-
-#%% varnames
+#%% Varnames
 ROOTPATH = 'E:/3-GitHub_BDehapiot/BD_VeinSeg/'
-NAME_cd31 = 'LCMVd2_MIP_cd31_RSize_sCrop.tif'
-NAME_iba1 = 'LCMVd2_MIP_iba1_RSize_sCrop.tif'
+NAME_cd31 = 'LCMVd2_MIP_cd31_sCrop.tif'
+NAME_iba1 = 'LCMVd2_MIP_iba1_sCrop.tif'
+NAME_mhc2 = 'LCMVd2_MIP_mhc2_sCrop.tif'
 
-PIXSIZE = 3.5425 # pixel size in µm
+PIXSIZE = 1.7712 # pixel size in µm (1.7712 for full size images)
+VEINSCAT = np.arange(0,30,5)  # define categories for veins in µm
+VEINSCAT_prox = 50 # distance for veins proximity maps in µm 
 
-THRESH_cd31 = 25 # threshold for binary
-MINSIZE_cd31 = 500 # minsize for binary objects
+THRESH_cd31 = 30 # threshold for binary
+MINSIZE_cd31 = 500 # minsize for binary objects in pixels (1000 for full size images)
+MAXFILT_cd31 = 14 # max filter size for veins radius in pixels (14 for full size images)
 
-THRESH_iba1 = 25 # threshold for binary
-MINSIZE_iba1 = 50 # minsize for binary objects
+THRESH_iba1 = 30 # threshold for binary 
+MINSIZE_iba1 = 50 # minsize for binary objects (100 for full size images)
+
+THRESH_mhc2 = 30 # threshold for binary 
+MINSIZE_mhc2 = 50 # minsize for binary objects (100 for full size images)
 
 #%% Open Stack from RAWNAME
 
 cd31 = io.imread(ROOTPATH+NAME_cd31)
 iba1 = io.imread(ROOTPATH+NAME_iba1)
+mhc2 = io.imread(ROOTPATH+NAME_mhc2)
 nY = cd31.shape[0] # Get Stack dimension (y)
 nX = cd31.shape[1] # Get Stack dimension (x)
 
-#%% Segment&process cd31 image (veins)
+#%% Segment & process cd31 image (veins)
 
 # Apply gaussian blur
-cd31_bin = gaussian(cd31, sigma=1, preserve_range=True)
+cd31_bin = gaussian(cd31, sigma=2, preserve_range=True)
 
 # Threshold image 
 cd31_bin = cd31_bin > THRESH_cd31
@@ -51,62 +53,75 @@ cd31_edmOut = distance_transform_edt(invert(cd31_bin))
 cd31_edmOut = cd31_edmOut * PIXSIZE # get distance in µm
 
 # Apply max filter
-cd31_edmIn_max = maximum_filter(cd31_edmIn, footprint=disk(5))
+cd31_edmIn_max = maximum_filter(cd31_edmIn, footprint=disk(MAXFILT_cd31))
 
 # Apply max filter
 cd31_edmIn_max[cd31_bin == 0] = 0
 
-#%% Segment&process iba1 image
+#%% Categorize veins and generate proximity maps
+
+nCat = VEINSCAT.size
+cd31_cat = np.zeros([nCat,nY,nX], dtype='bool')
+cd31_cat_edmOut = np.zeros([nCat,nY,nX], dtype='float32')
+cd31_cat_edmOut_proxmaps = np.zeros([nCat,nY,nX], dtype='bool')
+for i in range(nCat):
+    # Categorize veins
+    if i < nCat-1:
+        temp = (cd31_edmIn_max > VEINSCAT[i]) & (cd31_edmIn_max <= VEINSCAT[i+1])
+    else :
+        temp = cd31_edmIn_max > VEINSCAT[i]   
+        
+    # Pocess masks and get proximity maps  
+    temp = remove_small_objects(temp, min_size=MINSIZE_iba1)    
+    temp_edmOut = distance_transform_edt(invert(temp))   
+    temp_edmOut_proxmaps = (temp_edmOut <= VEINSCAT_prox/PIXSIZE) 
+    
+    # Append arrays
+    cd31_cat[i,:,:] = temp
+    cd31_cat_edmOut[i,:,:] = temp_edmOut    
+    cd31_cat_edmOut_proxmaps[i,:,:] = temp_edmOut_proxmaps
+
+#%% Segment & process iba1 and mhc2 images
 
 # Apply gaussian blur
-iba1_bin = gaussian(iba1, sigma=1, preserve_range=True)
+iba1_bin = gaussian(iba1, sigma=2, preserve_range=True)
+mhc2_bin = gaussian(mhc2, sigma=2, preserve_range=True)
 
 # Threshold image 
 iba1_bin = iba1_bin > THRESH_iba1
 iba1_bin = remove_small_objects(iba1_bin, min_size=MINSIZE_iba1)
+mhc2_bin = mhc2_bin > THRESH_mhc2
+mhc2_bin = remove_small_objects(mhc2_bin, min_size=MINSIZE_mhc2)
 
-#%% 
+#%% Measure 
 
-iba1_dist2veins = cd31_edmOut
-iba1_dist2veins[iba1_bin == 0] = ['nan']
-iba1_dist2veins = np.reshape(iba1_dist2veins, iba1_dist2veins.size)
-iba1_dist2veins = (iba1_dist2veins[~np.isnan(iba1_dist2veins)])
-
-plt.hist(iba1_dist2veins, bins=20, range=[0, 100], density=True, stacked=True);
-
-
-
-#%% Napari
-   
-# with napari.gui_qt():
-#     viewer = napari.view_image(cd31_bin).astype('uint8')*255
+proxmaps_area = np.zeros([nCat,5])
+for i in range(nCat):
     
+    temp_cd31_bin = np.copy(cd31_bin)
+    temp_proxmap = cd31_cat_edmOut_proxmaps[i,:,:]
+    
+    temp_iba1_bin = np.copy(iba1_bin)
+    temp_iba1_bin[temp_cd31_bin == 1] = 0 # remove iba1 signal overlapping veins
+    temp_iba1_bin[temp_proxmap == 0] = 0 # remove iba1 signal outside proxmap
+    
+    temp_mhc2_bin = np.copy(mhc2_bin)
+    temp_mhc2_bin[temp_cd31_bin == 1] = 0 # remove mhc2 signal overlapping veins
+    temp_mhc2_bin[temp_proxmap == 0] = 0 # remove mhc2 signal outside proxmap     
+    
+    proxmaps_area[i,0] = np.sum(temp_proxmap)*(PIXSIZE**2) # surface in µm²
+    proxmaps_area[i,1] = np.sum(temp_iba1_bin)*(PIXSIZE**2) # surface in µm²
+    proxmaps_area[i,2] = (proxmaps_area[i,1])/(proxmaps_area[i,0]) # proportion of iba1 signal in proximity of veins
+    proxmaps_area[i,3] = np.sum(temp_mhc2_bin)*(PIXSIZE**2) # surface in µm²
+    proxmaps_area[i,4] = (proxmaps_area[i,3])/(proxmaps_area[i,0]) # proportion of mhc2 signal in proximity of veins
+   
 #%% Saving
-io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_bin.tif', cd31_bin.astype('uint8')*255, check_contrast=True)
-io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_edmIn.tif', cd31_edmIn.astype('float32'), check_contrast=True)
-io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_edmOut.tif', cd31_edmOut.astype('float32'), check_contrast=True)
-io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_edmIn_max.tif', cd31_edmIn_max.astype('float32'), check_contrast=True)
-
-io.imsave(ROOTPATH+NAME_iba1[0:-4]+'_bin.tif', iba1_bin.astype('uint8')*255, check_contrast=True)
-
-io.imsave(ROOTPATH+NAME_iba1[0:-4]+'_dist2veins.tif', iba1_dist2veins.astype('float32'), check_contrast=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_bin.tif', cd31_bin.astype('uint8')*255, check_contrast=True, imagej=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_edmIn.tif', cd31_edmIn.astype('float32'), check_contrast=True, imagej=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_edmOut.tif', cd31_edmOut.astype('float32'), check_contrast=True, imagej=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_edmIn_max.tif', cd31_edmIn_max.astype('float32'), check_contrast=True, imagej=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_cat.tif', cd31_cat.astype('uint8')*255, check_contrast=True, imagej=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_cat_edmOut.tif', cd31_cat_edmOut.astype('float32'), check_contrast=True, imagej=True)
+io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_cat_edmOut_proxmaps.tif', cd31_cat_edmOut_proxmaps.astype('uint8')*255, check_contrast=True, imagej=True)
 
 #%% Current
-
-#%% apply sato filter
-
-# def sato_filter(im, sigmas):
-#     '''Enter function general description + arguments'''
-#     im_sato = sato(im,sigmas=sigmas,mode='reflect',black_ridges=False)   
-#     return im_sato
-
-# output_list = Parallel(n_jobs=35)(
-#     delayed(sato_filter)(
-#         cd31_bin,
-#         SATO_SIG[i]
-#         ) 
-#     for i in range(len(SATO_SIG))
-#     ) 
-
-# cd31_bin_sato = np.stack([arrays for arrays in output_list], axis=0)  
-# io.imsave(ROOTPATH+NAME_cd31[0:-4]+'_bin_sato.tif', cd31_bin_sato.astype('float32'), check_contrast=True)
